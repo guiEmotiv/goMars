7ab13d48746b43d4b8541dcb6470dc9b4bcdc026
Apple <guille_sjkc2@hotmail.com>
2016-12-10 02:38:07 -0500

    dia2

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-import serial
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from PyQt4 import QtGui
+import sys
+
+class VentanaPrincipal(QtGui.QWidget):
+
+    def __init__(self):
+        super(VentanaPrincipal,self).__init__()
+        self.iniUI()
+
+    def iniUI(self):
+        self.setGeometry(0,0,1920,1080)
+        self.setWindowTitle('INTERFACE NASA')
+        self.setWindowIcon(QtGui.QIcon('img/logo.jpg'))
+
+        self.show()
+
+    def closeEvent(self, QCloseEvent):
+
+        reply = QtGui.QMessageBox.question(self, 'Message',
+                                           "Are you sure to quit?", QtGui.QMessageBox.Yes |
+                                           QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            QCloseEvent.accept()
+        else:
+            QCloseEvent.ignore()
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = VentanaPrincipal()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
